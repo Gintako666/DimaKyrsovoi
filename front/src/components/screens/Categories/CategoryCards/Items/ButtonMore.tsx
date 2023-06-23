e36067e12b52c78167e04a2e9ebb6f0f9c329c01
Maksym Davydenko <maksydenko@gmail.com>
2023-06-23 17:23:58 +0300

    Improve button more

--- front/src/components/screens/Categories/CategoryCards/Items/ButtonMore.tsx
+++ front/src/components/screens/Categories/CategoryCards/Items/ButtonMore.tsx
@@ -1,11 +1,42 @@
-import { FC, useRef } from 'react';
+import { FC, useRef, useEffect } from 'react';
 import Img from '~/components/base/Img/Img';
 import more from '~/assets/img/icons/more.svg';
 
 const ButtonMore: FC = () => {
   const buttonRef = useRef<HTMLButtonElement>(null);
 
-  const handleButtonClick = () => {
+  useEffect(() => {
+    // Handle outside click
+    interface IHandleOutsideClick {
+      (e: MouseEvent): void
+    }
+    const handleClickOutside: IHandleOutsideClick = (e) => {
+      const buttonElement = buttonRef.current;
+      const target = e.target as HTMLElement;
+
+      if (buttonElement && !buttonElement.contains(target)) {
+        const activeElements = document.querySelectorAll('.category-cards__more_active');
+
+        activeElements.forEach((element) => {
+          element.classList.remove('category-cards__more_active');
+        });
+      }
+    };
+
+    document.addEventListener('click', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, []);
+
+  // Handle click button
+  interface IHandleClickButton {
+    (e: React.MouseEvent<HTMLButtonElement>): void
+  }
+  const handleClickButton: IHandleClickButton = (e) => {
+    e.stopPropagation();
+
     const buttonElement = buttonRef.current;
 
     if (buttonElement) {
@@ -28,7 +59,7 @@ const ButtonMore: FC = () => {
     <button
       type="button"
       className="category-cards__more"
-      onClick={ handleButtonClick }
+      onClick={ handleClickButton }
       ref={ buttonRef }
     >
       <Img className="category-cards" img={ img } resetStyle />
