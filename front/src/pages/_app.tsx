16109eda0358d7f732c3f66f6c9d050a1bbaa40e
Maksym Davydenko <maksydenko@gmail.com>
2023-07-12 14:43:48 +0300

    AuthCheck refactoring

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -1,8 +1,8 @@
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
+import AuthCheck from '~/components/base/AuthCheck/AuthCheck';
 import { UserProvider } from '~/contexts/user';
-import AuthCheck from '~/components/base/AuthCheck';
 
 import '~/assets/scss/globals.scss';
 
