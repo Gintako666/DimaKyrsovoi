dfd9c185884e4cb108322e5099b3f51c4808e85a
Gintako666 <dasha8854567@gmail.com>
2023-07-12 12:13:01 +0300

    fix chart interfaces

--- front/src/interfaces/chart.interface.ts
+++ front/src/interfaces/chart.interface.ts
@@ -2,9 +2,9 @@ export interface Dataset {
   label: string,
   data: (number | null)[],
   borderColor: string,
-};
+}
 
 export interface DataToChart {
   labels: string[];
   datasets: Dataset[];
-};
+}
