ee55a5238bf0378cc044b5d3c578ce064e200aee
Maksym Davydenko <maksydenko@gmail.com>
2023-07-12 15:04:43 +0300

    Add Popup and Transition

---
+++ front/src/components/base/Popup/Body.tsx
@@ -0,0 +1,35 @@
+import { FC, ReactNode, MouseEvent, useRef } from "react";
+
+interface BodyProps {
+  children: ReactNode;
+  onClick(): void;
+}
+
+const Body: FC<BodyProps> = ({ children, onClick }) => {
+  const popupBoxRef = useRef<HTMLDivElement>(null);
+
+  // Handle click
+  interface IHandleClick {
+    ({ target }: MouseEvent<EventTarget>): false | void;
+  }
+  const handleClick: IHandleClick = ({ target }) => {
+    const popupBoxElement = popupBoxRef.current;
+
+    if (!popupBoxElement?.contains(target as Node)) {
+      onClick();
+    }
+  };
+
+  return (
+    <div className="popup__body" onClick={handleClick}>
+      <div className="popup__content">
+        <div className="popup__box" ref={popupBoxRef}>
+          <button className="popup__cross" onClick={onClick}></button>
+          <div className="popup__children">{children}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Body;
---
+++ front/src/components/base/Popup/Popup.tsx
@@ -0,0 +1,42 @@
+import { FC, ReactNode } from "react";
+
+import Transition from "@/components/base//Transition/Transition";
+import Body from "./Body";
+
+import { useScrollLock } from "@/hooks/useScrollLock";
+
+import { handleClassName } from "@/utils/className.util";
+
+interface PopupProps {
+  className: string;
+  modifier?: string;
+  children: ReactNode;
+  button: ReactNode;
+}
+
+const Popup: FC<PopupProps> = ({ className, modifier, children, button }) => {
+  const { isScrollLocked, setIsScrollLocked } = useScrollLock();
+
+  const handleClick = () => {
+    setIsScrollLocked(!isScrollLocked);
+  };
+
+  const modifiedClassName = handleClassName(
+    !!modifier,
+    `${className}__popup`,
+    modifier
+  );
+
+  return (
+    <div className={`${modifiedClassName} popup`}>
+      <button className="popup__button" onClick={handleClick}>
+        {button}
+      </button>
+      <Transition condition={isScrollLocked} className="popup">
+        <Body onClick={handleClick}>{children}</Body>
+      </Transition>
+    </div>
+  );
+};
+
+export default Popup;
