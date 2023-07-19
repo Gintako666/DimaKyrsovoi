3ec26f57c9c2c4f094510a387d93eb87a073dbd0
Maksym Davydenko <maksydenko@gmail.com>
2023-07-19 18:42:38 +0300

    Minor CategoryCard fix

--- front/src/components/screens/Categories/CategoryCards/CategoryCards.tsx
+++ front/src/components/screens/Categories/CategoryCards/CategoryCards.tsx
@@ -5,13 +5,15 @@ import Loader from '~/components/shared/Loader/Loader';
 import useFetchData from '~/hooks/useFetchData';
 
 import CategoriesService from '~/services/categories.service';
+
+import { ICategory } from '~/interfaces/category.interface';
 import Items from './Items/Items';
 
 const CategoryCards: FC = () => {
   const { getCategories } = CategoriesService;
   const { data, isLoading, error } = useFetchData(getCategories);
 
-  const categories = data?.data;
+  const categories: ICategory[] = data?.data;
 
   if (isLoading) {
     return <Loader />;
